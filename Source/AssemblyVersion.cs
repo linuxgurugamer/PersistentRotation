67e3525275aafd8331ed22f67c9e46047a7a7105
Jonathan Bayer <linuxgurugamer@gmail.com>
2024-01-01 09:04:49 -0500

    Removed Mechjeb dependency , in csproj file

--- Source/AssemblyVersion.cs
+++ Source/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.9.1.8")]
- [assembly: AssemblyFileVersion("1.9.1.8")]
+ [assembly: AssemblyVersion("1.9.1.9")]
+ [assembly: AssemblyFileVersion("1.9.1.9")]
